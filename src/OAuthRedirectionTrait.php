71078e56ae4aef6fd092221acf0e78ad5bb97e70
namelivia <namelivia@users.noreply.github.com>
2020-11-01 13:41:08 +0000

    Apply fixes from StyleCI

--- src/OAuthRedirectionTrait.php
+++ src/OAuthRedirectionTrait.php
@@ -4,18 +4,18 @@ declare(strict_types=1);
 
 namespace Namelivia\TravelPerk\Laravel;
 
-use Throwable;
-use Namelivia\TravelPerk\OAuth\MissingCodeException;
-use Namelivia\TravelPerk\Laravel\Facades\TravelPerk;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Request;
+use Namelivia\TravelPerk\Laravel\Facades\TravelPerk;
+use Namelivia\TravelPerk\OAuth\MissingCodeException;
+use Throwable;
 
-trait OAuthRedirectionTrait {
-
-    static function handleOAuthRedirection(Throwable $exception) {
+trait OAuthRedirectionTrait
+{
+    public static function handleOAuthRedirection(Throwable $exception)
+    {
         if (is_a($exception, MissingCodeException::class)) {
             return Redirect::to(TravelPerk::getAuthUri(Request::path()));
         }
     }
-
 }
